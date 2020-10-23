4e69f75990e19a2b9368b7eaf3e4d83a40f0f056
Andrew Tan <andrewtan89@gmail.com>
2020-10-24 00:46:12 +0800

    inital commit version 0.0.2

---
+++ README.md
@@ -0,0 +1,31 @@
+# bible-vod
+
+bible-vod is an extension that displays the Bible Gateway Verse of the Day (VOTD) on VS code startup, or when triggered by a command. This redirects to BibleGateway.com, but is in no way affiliated with BibleGateway.
+
+## Features
+
+The dialog bar will show the verse on **startup**, with an option to read the full chapter:
+
+![VOTD on starup](https://i.imgur.com/nWelW4n.gif)
+
+
+or on **command** 'Show Verse of the Day':
+
+![VOTD on command 'Show Verse of the Day'](https://i.imgur.com/GxfMflP.gif)
+
+
+## Extension Settings
+
+There are currently no settings but there is the intention to enable configuring the Bible version in the near future, and to allow setting when VOTD should be shown (e.g. startup, at regular time of day, etc).
+
+## Release Notes
+
+### 0.0.2
+
+Updated links to GIFs demonstrating features
+
+### 0.0.1
+
+Initial release of bible-vod
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,61 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import axios from 'axios';
+
+// Get JSON of vod from below URL
+async function getVod(): Promise<void> {
+
+  const url: string = 'https://www.biblegateway.com/votd/get/';
+
+  try {
+    const res = await axios.get(url, {
+      params: {
+        format: 'json',
+        version: 'ESV' // Make customizable in future
+      }
+    });
+
+    vscode.window.showInformationMessage('VOTD: ' + res.data.votd.content + ' (' + res.data.votd.reference + ')',
+    'Show Full Chapter').then(selection => {
+      if (selection === 'Show Full Chapter') {
+
+        let reference = res.data.votd.reference;
+        let chapter = reference.split(":", 1);
+        let chapterUri = 'https://www.biblegateway.com/passage/?search=' + chapter[0] + '&version=ESV';
+        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(chapterUri));
+      } 
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "bible-vod" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('bible-vod.showVod', () => {
+  
+    // The code you place here will be executed every time your command is executed
+    getVod();
+  
+  });
+  
+  let startupMessage = vscode.commands.registerCommand('onStartupFinished', () => {
+    getVod();
+  });
+
+  context.subscriptions.push(disposable);
+  vscode.commands.executeCommand('onStartupFinished');
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
